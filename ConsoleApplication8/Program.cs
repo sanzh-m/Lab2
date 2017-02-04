4461a590e0254c1b2bf4e6aa470902a14a49b899
sanzh-m <sanzh98@gmail.com>
2017-02-04 14:11:28 +0600

    Add project files.

---
+++ ConsoleApplication8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"input.txt");
+            string[] numbers = text.Split(' ');
+            int[] a = new int[numbers.Length];
+            for (int i = 0; i< numbers.Length; ++i)
+            {
+                a[i] = Convert.ToInt32(numbers[i]);
+            }
+            int max, min;
+            max = int.MinValue;
+            min = int.MaxValue;
+            for (int i = 0; i < numbers.Length; ++i)
+            {
+                if (max < a[i]) max = a[i];
+                if (min > a[i]) min = a[i];
+            }
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+            Console.ReadKey();
+        }
+    }
+}
